ac7d437eb20f1675d91c993dd7198dc8e42f32e6
Martin Roesch <martin.roesch@kistler.com>
2020-11-06 18:00:14 +0100

    Format source code.

--- src/main.c
+++ src/main.c
@@ -4,39 +4,37 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-#include <zephyr.h>
+#include <drivers/uart.h>
+#include <string.h>
 #include <sys/printk.h>
 #include <sys/util.h>
-#include <string.h>
 #include <usb/usb_device.h>
-#include <drivers/uart.h>
+#include <zephyr.h>
 
-void main(void)
-{
-	const struct device *dev = device_get_binding(
-			CONFIG_UART_CONSOLE_ON_DEV_NAME);
-	uint32_t dtr = 0;
+void main(void) {
+  const struct device *dev =
+      device_get_binding(CONFIG_UART_CONSOLE_ON_DEV_NAME);
+  uint32_t dtr = 0;
 
-	if (usb_enable(NULL)) {
-		return;
-	}
+  if (usb_enable(NULL)) {
+    return;
+  }
 
-	/* Poll if the DTR flag was set, optional */
-	while (!dtr) {
-		uart_line_ctrl_get(dev, UART_LINE_CTRL_DTR, &dtr);
-	}
+  /* Poll if the DTR flag was set, optional */
+  while (!dtr) {
+    uart_line_ctrl_get(dev, UART_LINE_CTRL_DTR, &dtr);
+  }
 
-	if (strlen(CONFIG_UART_CONSOLE_ON_DEV_NAME) !=
-	    strlen("CDC_ACM_0") ||
-	    strncmp(CONFIG_UART_CONSOLE_ON_DEV_NAME, "CDC_ACM_0",
-		    strlen(CONFIG_UART_CONSOLE_ON_DEV_NAME))) {
-		printk("Error: Console device name is not USB ACM\n");
+  if (strlen(CONFIG_UART_CONSOLE_ON_DEV_NAME) != strlen("CDC_ACM_0") ||
+      strncmp(CONFIG_UART_CONSOLE_ON_DEV_NAME, "CDC_ACM_0",
+              strlen(CONFIG_UART_CONSOLE_ON_DEV_NAME))) {
+    printk("Error: Console device name is not USB ACM\n");
 
-		return;
-	}
+    return;
+  }
 
-	while (1) {
-		printk("Hello World! %s\n", CONFIG_ARCH);
-		k_sleep(K_SECONDS(1));
-	}
+  while (1) {
+    printk("Hello World! %s\n", CONFIG_ARCH);
+    k_sleep(K_SECONDS(1));
+  }
 }
