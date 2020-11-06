0b87764742102510f1d21a151492185d53110bf0
Martin Roesch <martin.roesch@kistler.com>
2020-11-06 19:27:02 +0100

    Enable and initialize bluetooth.

--- src/main.c
+++ src/main.c
@@ -4,6 +4,11 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+#include <bluetooth/bluetooth.h>
+#include <bluetooth/conn.h>
+#include <bluetooth/gatt.h>
+#include <bluetooth/hci.h>
+#include <bluetooth/uuid.h>
 #include <drivers/uart.h>
 #include <string.h>
 #include <sys/printk.h>
@@ -11,10 +16,48 @@
 #include <usb/usb_device.h>
 #include <zephyr.h>
 
+static const struct bt_data ad[] = {
+    BT_DATA_BYTES(BT_DATA_FLAGS, (BT_LE_AD_GENERAL | BT_LE_AD_NO_BREDR)),
+    BT_DATA_BYTES(BT_DATA_UUID128_ALL, 0xf0, 0xde, 0xbc, 0x9a, 0x78, 0x56, 0x34,
+                  0x12, 0x78, 0x56, 0x34, 0x12, 0x78, 0x56, 0x34, 0x12),
+};
+
+static void connected(struct bt_conn *conn, uint8_t err) {
+  if (err) {
+    printk("Connection failed (err 0x%02x)\n", err);
+  } else {
+    printk("Connected\n");
+  }
+}
+
+static void disconnected(struct bt_conn *conn, uint8_t reason) {
+  printk("Disconnected (reason 0x%02x)\n", reason);
+}
+
+static bool le_param_req(struct bt_conn *conn, struct bt_le_conn_param *param) {
+  printk("Connection parameters update requested\n");
+  return true;
+}
+
+static void le_param_updated(struct bt_conn *conn, uint16_t interval,
+                             uint16_t latency, uint16_t timeout) {
+  printk(
+      "Connection parameters updated: interval: %d, latency: %d, timeout: %d\n",
+      interval, latency, timeout);
+}
+
+static struct bt_conn_cb conn_callbacks = {
+    .connected = connected,
+    .disconnected = disconnected,
+    .le_param_req = le_param_req,
+    .le_param_updated = le_param_updated,
+};
+
 void main(void) {
   const struct device *dev =
       device_get_binding(CONFIG_UART_CONSOLE_ON_DEV_NAME);
   uint32_t dtr = 0;
+  int err;
 
   if (usb_enable(NULL)) {
     return;
@@ -33,8 +76,20 @@ void main(void) {
     return;
   }
 
-  while (1) {
-    printk("Hello World! %s\n", CONFIG_ARCH);
-    k_sleep(K_SECONDS(1));
+  /* Setup Bluetooth */
+  bt_conn_cb_register(&conn_callbacks);
+
+  err = bt_enable(NULL);
+  if (err) {
+    printk("Bluetooth init failed (err %d)\n", err);
+    return;
   }
+  printk("Bluetooth initialized\n");
+
+  err = bt_le_adv_start(BT_LE_ADV_CONN_NAME, ad, ARRAY_SIZE(ad), NULL, 0);
+  if (err) {
+    printk("Advertising failed to start (err %d)\n", err);
+    return;
+  }
+  printk("Advertising successfully started\n");
 }
